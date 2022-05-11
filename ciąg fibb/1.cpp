655b480bfdefa78641df72ca7afc6578c3c39e34
drpinio <jakub.pinczewski@vp.pl>
2022-05-11 14:01:56 +0200

    komentarz

---
+++ ciąg fibb/1.cpp
@@ -0,0 +1,28 @@
+﻿// 1.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, i;
+    a = 1;
+    b = 1;
+    c = a + b;
+    i = 1;
+    int x;
+    cout << "podaj x" << endl;
+    cin >> x;
+    do
+    {
+        a = b;
+        b = c;
+        c = a + b;
+        i++;
+        cout << "Fibb" << "=" << a << endl;
+    } while (x != i);
+    return 0;
+}
+
+
---
+++ nww i nwd/2.cpp
@@ -0,0 +1,41 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int nwd(int x, int y)
+{
+    do
+    {
+        if (x > y)
+        {
+            x = x - y;
+        }
+        if (y > x)
+        {
+            y = y - x;
+        }
+
+    } while (x != y);
+    return x;
+}
+int nww(int x, int y)
+{
+    int c;
+    c = x * y / nwd(x, y);
+    return c;
+
+}
+int main()
+{
+    int a = 0;
+    int b = 0;
+    cout << "Podaj a" << endl;
+    cin >> a;
+    cout << "Podaj b" << endl;
+    cin >> b;
+    cout << "Nwd to" << nwd(a, b) << endl;
+    cout << "Nww to" << nww(a, b) << endl;
+    return 0;
+}
+
+
